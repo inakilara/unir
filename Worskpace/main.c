fb21fee6a03dfc84e9476fbaab0bf502a200be88
Iñaki Lara <inaki@turingcapital.group>
2026-01-07 20:45:11 +0100

    Changes

---
+++ Worskpace/main.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(){
+  FILE *fichero = fopen("../Worskpace/Lore_ipsum.txt","r");
+  if (fichero==NULL){
+    printf("Error abriendo el archivo");
+    return -1;
+  }
+  /*char c;
+  do{
+    c = fgetc(fichero);
+    putc(c,stdout);
+  }while(c!=EOF);*/
+  char *texto = NULL;
+  int len=1;
+  int c;
+  while((c = fgetc(fichero)) != EOF){
+    if (c!=EOF) {
+      len++;
+      char *temp = (char*) realloc(texto,len*sizeof(char));
+      if (temp==NULL) {
+        free(texto);
+        fclose(fichero);
+        return -1;
+      }
+        texto = temp;
+        texto[len-2]=(char)c;
+    }
+  }
+  if (texto!=NULL){
+     texto[len-1] = '\0';
+  }
+  printf("%s",texto);
+  free(texto);
+  fclose(fichero);
+  return 0;
+}
